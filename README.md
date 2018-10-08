dde264af7e2758d66c64765a78f505f168a690f8
plasticuproject <nicholes.bowling@gmail.com>
2018-10-08 15:27:26 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # hibpwned
-Python API wrapper for haveibeenpwned.com
+A friendly, low-level, fully functional, Python API wrapper for haveibeenpwned.com <br/>
+All data sourced from https://haveibeenpwned.com <br/>
+Visit https://haveibeenpwned.com/API/v2 to read the Acceptable Use Policy <br/>
+for rules regarding acceptable usage of this API. <br/>
+
+
+## Instalation
+
+```
+pip install hibpwned
+```
+
+## Usage
+This module contains the class Pwned with functions: <br/>
+
+searchAllBreaches <br/>
+allBreaches <br/>
+singleBreach <br/>
+dataClasses <br/>
+searchPastes <br/>
+searchHashes <br/>
+
+All functions return a JSON object containing relevent data, with the exception <br/>
+of searchHashes, which returns a string object.
+
+See module DocStrings for function descriptions and parameters <br/>
+
+## Examples
+```python
+myApp = Pwned('test@example.com', 'My_App')
+myBreaches = myApp.searchAllBreaches()
+Breaches = myApp.allBreaches()
+adobe = myApp.singleBreach('adobe')
+data = myApp.dataClasses()
+myPastes = myApp.searchPastes()
+myHashes = myApp.searchHashes('21BD1')
+```
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+version = '1.0.0'
+
+setup(name='hibpwned',
+      packages=['hibpwned'],
+      version=version,
+      description='A human friendly Python API wrapper for haveibeenpwned.com',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      author='plasticuproject',
+      author_email='plasticuproject@pm.me',
+      url='http://github.com/plasticuproject/hibpwned',
+      download_url='https://github.com/plasticuproject/hibpwned/archive/v' + version + '.tar.gz',
+      keywords=['hibp', 'haveibeenpwned', 'api', 'wrapper'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: '
+          'GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 3',
+          'Topic :: Communications :: Chat',
+          'Topic :: Utilities'
+      ],
+      license='GPLv3',
+      install_requires=['requests'],
+      zip_safe=False,
+include_package_data=True)
