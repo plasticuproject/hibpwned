eb15a4b66ea9aea394d7bba42e3e0a4e1e0cdd60
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2022-01-27 22:22:51 -0500

    Fixed code smells, disabled silly lgtm alers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """Setup file"""
 from setuptools import setup  # type: ignore
 
-VERSION = "1.3.0"
+VERSION = "1.3.1"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
